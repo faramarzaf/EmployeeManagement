582a7e1e255db9aa81ac1532e2071072d13f8865
FaramarzAf <faramarzafzali@gmail.com>
2021-04-01 01:20:14 +0430

    exception handler added

--- src/main/java/com/spring/web/app/EmployeeManagementWebApp/EmployeeManagementWebApplication.java
+++ src/main/java/com/spring/web/app/EmployeeManagementWebApp/EmployeeManagementWebApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeeManagementWebAppApplication {
+public class EmployeeManagementWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeManagementWebAppApplication.class, args);
+		SpringApplication.run(EmployeeManagementWebApplication.class, args);
 	}
 
 }
---
+++ src/main/java/com/spring/web/app/EmployeeManagementWebApp/ExceptionController.java
@@ -0,0 +1,29 @@
+package com.spring.web.app.EmployeeManagementWebApp;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class ExceptionController {
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(value = {NoHandlerFoundException.class})
+    public ResponseEntity<String> handlerNotFoundException(NoHandlerFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+}
